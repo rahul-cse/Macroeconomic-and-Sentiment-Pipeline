5b0dd354b792e0c8e725b6d6c1cf3679edb2ca10
rahul barua <rahul.ctg62@gmail.com>
2026-07-14 00:07:59 +0200

    added fred fetcher

--- fetchers/fetcher.py
+++ fetchers/fetcher.py
@@ -1,16 +1,17 @@
 import yfinance as yf
 import pandas as pd
-#from fredapi import Fred
+from fredapi import Fred
 from loguru import logger
 
+fred = Fred(api_key="FRED_API_KEY")
 
 def fetch_factor(name: str, source: str, ticker: str):
 
     if source == "yahoo":
         return fetch_yahoo(name, ticker)
 
-    # elif source == "fred":
-    #     return fetch_fred(ticker)
+    elif source == "fred":
+        return fetch_fred(name,ticker)
 
     else:
         raise ValueError(f"Unknown source: {source}")
@@ -28,4 +29,17 @@ def fetch_yahoo(name:str, ticker: str, start="2010-01-01"):
     df.columns = [name]
     df.index = pd.to_datetime(df.index)
 
-    return df       
+    return df  
+
+def fetch_fred(name:str, ticker: str, start="2010-01-01"):
+    logger.info(f"Fred: {ticker}") 
+    try:
+        series = fred.get_series(ticker)
+        df = pd.DataFrame(series)
+        df.columns = [name]
+        df.index = pd.to_datetime(df.index)
+        df = df[df.index >= start]
+        return df
+    except Exception as e:
+        logger.error(f"Failed to fetch FRED series '{ticker}': {e}")
+        return pd.DataFrame()   
